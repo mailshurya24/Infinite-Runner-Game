9c60194cecb27de792cb69ea93891815a1410335
mailshurya24 <70964703+mailshurya24@users.noreply.github.com>
2020-12-28 12:49:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ var player, player_running;
 var foodGroup, bananaimage;
 var obstacleGroup, obstacleimage;
 var score;
+var gameState, PLAY, END;
 
 function preload()
 {
@@ -18,20 +19,21 @@ function setup()
 {
   createCanvas(600,600);
   
-  backdrop = createSprite(0,0,600,200);
+  backdrop = createSprite(300,300,600,200);
   backdrop.addImage(backImage);
   backdrop.scale = 1.5;
-  backdrop.x = backdrop.width /2;
+ // backdrop.x = backdrop.width /2;
   backdrop.velocityX = -6;
   
-  player = createSprite(300,140,5,5);
+  player = createSprite(200,140,5,5);
   player.addAnimation("running",player_running);
   player.scale= 0.08;
   player.velocityX = 4;
 
   
-  ground = createSprite(600,590,600,20);
-  ground.x = ground.width/2;
+  ground = createSprite(600,590,1200,20);
+  ground.velocityX = -6;
+  //ground.x = ground.width/2;
   ground.visible = false;
   
   foodGroup = new Group();
@@ -47,19 +49,22 @@ function draw()
   camera.position.x = player.x
   camera.position.y = 300;
   
+if(gameState===PLAY)
+{
   //resetting the groundback to the center
-  if (backdrop.x < 0)
+ if (backdrop.x < camera.position.x)
   {
-     backdrop.x = backdrop.width/2;
+     backdrop.x = camera.position.x+400;
   }
-
-  if (ground.x < 0)
-  {
-    ground.x = ground.width/2;
-  }
+  //console.log(ground.x)
+ // if (ground.x < 0)
+ // {
+    //console.log(ground.width)
+    ground.x = camera.position.x;
+ // }
   
   //make the monkey jump
-  if(keyDown("space"))
+ if(keyDown("space"))
   {
    player.velocityY = -12; 
   }
@@ -72,7 +77,7 @@ function draw()
     foodGroup.destroyEach();
     score = score + 2;
   }
-  
+ 
   switch (score)
   {
     case 10: player.scale = 0.12;
@@ -86,26 +91,45 @@ function draw()
             
     case 40: player.scale = 0.18;
             break;  
+
+    case 200: player.scale = 0.3;
+              break;        
     
             default: break;
-  }
-  
-  if(obstacleGroup.isTouching(player))
-  {
-    player.scale = 0.08;
   }
   
   player.collide(ground);
   
   spawnFood();
   spawnObstacles();
-  
+}
+ 
+if(obstacleGroup.isTouching(player))
+  { gameState===END;
+    player.scale = 0.08;
+  }
+
+  if(gameState===END && gameState!== PLAY)
+  {
+      ground.velocityX=0;
+      backdrop.velocityX=0;
+      text("YOU LOSE!",camera.position.x,300);    
+      text("Your Score = "+score,camera.position.x,320);
+      text("Press R to replay",camera.position.x,340);
+  }
+
+  if(keyDown('r'))
+  {
+    gameState===PLAY;
+  }
+
+
   drawSprites();
   
   stroke("white");
   textSize(20);
   fill("white");
-  text("Score: "+ score,500,50);
+  text("Score: "+ score,camera.position.x+100,50);
 }
 
 function spawnFood()
@@ -113,13 +137,14 @@ function spawnFood()
   if(frameCount%80===0)
   {
      var banana=createSprite(600,200,10,10);
-     banana.y=Math.round(random(80,120));
+     banana.x = camera.position.x+400;
+     banana.y=Math.round(random(500,560));
 
      banana.addImage(bananaimage);
      banana.scale=0.05;
 
-  //   banana.velocityX=-5;
-     banana.lifetime=80;
+     banana.velocityX=-5;
+     banana.lifetime=150;
 
      foodGroup.add(banana);
   }
@@ -129,11 +154,12 @@ function spawnObstacles()
 {
   if(frameCount%300===0)
   {
-    var obstacle=createSprite(600,135,40,10);
+    var obstacle=createSprite(600,550,40,10);
+    obstacle.x = camera.position.x+400;
     obstacle.addImage(obstacleimage);
     obstacle.scale=0.15;
 
-  //  obstacle.velocityX=-8;
+    obstacle.velocityX=-8;
     obstacle.lifetime=200;
 
     obstacleGroup.add(obstacle);
